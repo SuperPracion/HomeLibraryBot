f527e4e67253fe79e3c9b85e63904e499fa73ec8
SuperPracion <pracion@bk.ru>
2024-08-17 23:36:28 +0300

    Взаимодействие с базой данной вынесено в декоратор get_cursor

--- main.py
+++ main.py
@@ -3,10 +3,19 @@ import config
 from telegram import Update
 from telegram.ext import Application, CallbackContext, CommandHandler
 
-# Функция для подключения к БД
-def get_db_connection():
-    conn = sqlite3.connect("book_catalog.db")
-    return conn
+
+# Декоратор работы с бд
+def get_cursor(func):
+    async def wrapper(*args, **kwargs):
+        conn = sqlite3.connect("book_catalog.db")
+        cursor = conn.cursor()
+        kwargs['cursor'] = cursor
+        res = await func(*args, **kwargs)
+        conn.commit()
+        conn.close()
+        return res
+
+    return wrapper
 
 
 # Команда /start
@@ -17,15 +26,14 @@ async def start(update: Update, context: CallbackContext):
         "редактировать/удалять свои книги с помощью команд /edit и /delete."
     )
 
+
 # Просмотр каталога
-async def catalog(update: Update, context: CallbackContext):
-    conn = get_db_connection()
-    cursor = conn.cursor()
+@get_cursor
+async def catalog(update: Update, context: CallbackContext, cursor: sqlite3.Cursor):
     cursor.execute("SELECT id, name, holder FROM books")
     books = cursor.fetchall()
-    conn.close()
 
-    if len(books) == 0:
+    if not books:
         await update.message.reply_text("Каталог пуст.")
     else:
         catalog_list = "\n".join(
@@ -33,13 +41,12 @@ async def catalog(update: Update, context: CallbackContext):
         )
         await update.message.reply_text(f"Книги в каталоге:\n{catalog_list}")
 
+
 # Вспомогательная функция для add_book. Получение идентификатора дял следующей книги
-def get_next_id():
-    conn = get_db_connection()
-    cursor = conn.cursor()
+@get_cursor
+async def get_next_id(cursor: sqlite3.Cursor):
     cursor.execute("SELECT id FROM books ORDER BY id ASC")
     ids = cursor.fetchall()
-    conn.close()
 
     # Ищем минимально пропущенный ID
     expected_id = 1
@@ -51,8 +58,10 @@ def get_next_id():
 
     return expected_id
 
+
 # Добавление книги
-async def add_book(update: Update, context: CallbackContext):
+@get_cursor
+async def add_book(update: Update, context: CallbackContext, cursor: sqlite3.Cursor):
     if len(context.args) == 0:
         await update.message.reply_text("Используйте /add <название книги> для добавления книги.")
         return
@@ -60,8 +69,6 @@ async def add_book(update: Update, context: CallbackContext):
     book_name = " ".join(context.args)
     user = update.message.from_user.username
 
-    conn = get_db_connection()
-    cursor = conn.cursor()
     cursor.execute("SELECT holder FROM books")
     holders = [holder[0] for holder in cursor.fetchall()]
 
@@ -70,15 +77,14 @@ async def add_book(update: Update, context: CallbackContext):
     elif holders.count(user) > config.max_books_per_user:
         await update.message.reply_text("Превышен лимит книг на одного пользователя.")
     else:
-        cursor.execute("INSERT INTO books (id, name, holder) VALUES (?, ?, ?)",(get_next_id(), book_name, user),)
-        conn.commit()
-
+        cursor.execute("INSERT INTO books (id, name, holder) VALUES (?, ?, ?)",
+                       (await get_next_id(), book_name, user), )
         await update.message.reply_text(f"Книга '{book_name}' была добавлена в каталог под вашим именем!")
 
-    conn.close()
 
 # Редактирование книги
-async def edit_book(update: Update, context: CallbackContext):
+@get_cursor
+async def edit_book(update: Update, context: CallbackContext, cursor: sqlite3.Cursor):
     if len(context.args) < 2:
         await update.message.reply_text("Используйте /edit <ID книги> <новое название книги> для редактирования.")
         return
@@ -87,31 +93,27 @@ async def edit_book(update: Update, context: CallbackContext):
     new_name = " ".join(context.args[1:])
     user = update.message.from_user.username
 
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute("SELECT holder, name FROM books WHERE id = ?",(book_id,))
+    cursor.execute("SELECT holder, name FROM books WHERE id = ?", (book_id,))
     book = cursor.fetchone()
 
     # Если книги нет в бд
     if book is None:
         await update.message.reply_text("Книга с таким ID не найдена.")
 
-    # Если текущий пользоваель не тот, кто редактирует
+    # Если текущий пользователь не тот, кто редактирует
     if book[0] != user:
         await update.message.reply_text("Вы не являетесь держателем этой книги и не можете её редактировать.")
     elif book[0] == user:
         if book[1] == new_name:
             await update.message.reply_text("Изменений не обнаружено. Книга уже имеет это название.")
         else:
-            cursor.execute("UPDATE books SET name = ? WHERE id = ?",(new_name, book_id))
-            conn.commit()
+            cursor.execute("UPDATE books SET name = ? WHERE id = ?", (new_name, book_id))
             await update.message.reply_text(f"Книга с ID {book_id} была обновлена.")
-
-    conn.close()
 
 
 # Удаление книги
-async def delete_book(update: Update, context: CallbackContext):
+@get_cursor
+async def delete_book(update: Update, context: CallbackContext, cursor: sqlite3.Cursor):
     if len(context.args) == 0:
         await update.message.reply_text("Используйте /delete <ID книги> для удаления книги.")
         return
@@ -119,8 +121,6 @@ async def delete_book(update: Update, context: CallbackContext):
     book_id = context.args[0]
     user = update.message.from_user.username
 
-    conn = get_db_connection()
-    cursor = conn.cursor()
     cursor.execute("SELECT holder FROM books WHERE id = ?", (book_id,))
     book = cursor.fetchone()
 
@@ -128,15 +128,14 @@ async def delete_book(update: Update, context: CallbackContext):
         await update.message.reply_text("Книга с таким ID не найдена.")
     elif book[0] == user:
         cursor.execute("DELETE FROM books WHERE id = ?", (book_id,))
-        conn.commit()
         await update.message.reply_text(f"Книга с ID {book_id} была удалена из каталога.")
     else:
         await update.message.reply_text("Вы не являетесь держателем этой книги и не можете её удалить.")
 
-    conn.close()
 
 # Взять книгу
-async def take_book(update: Update, context: CallbackContext):
+@get_cursor
+async def take_book(update: Update, context: CallbackContext, cursor: sqlite3.Cursor):
     if len(context.args) == 0:
         await update.message.reply_text("Используйте /take <ID книги> для того, чтобы стать держателем.")
         return
@@ -144,8 +143,6 @@ async def take_book(update: Update, context: CallbackContext):
     book_id = context.args[0]
     user = update.message.from_user.username
 
-    conn = get_db_connection()
-    cursor = conn.cursor()
     cursor.execute("SELECT holder FROM books WHERE id = ?", (book_id,))
     book = cursor.fetchone()
 
@@ -154,11 +151,9 @@ async def take_book(update: Update, context: CallbackContext):
     elif book[0] == user:
         await update.message.reply_text("Вы уже являетесь держателем этой книги.")
     else:
-        cursor.execute("UPDATE books SET holder = ? WHERE id = ?",(user, book_id))
-        conn.commit()
+        cursor.execute("UPDATE books SET holder = ? WHERE id = ?", (user, book_id))
         await update.message.reply_text(f"Вы теперь держатель книги с ID {book_id}.")
 
-    conn.close()
 
 # Основная функция
 application = (Application.builder().token(config.token_name).build())
