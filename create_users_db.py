21fa9f35ef6a51d0cedd1ea4887453b086411c80
SuperPracion <pracion@bk.ru>
2024-08-18 13:38:44 +0300

    Добавление признака уникальности на поля id и chat_id

--- create_users_db.py
+++ create_users_db.py
@@ -4,12 +4,13 @@ def create_db():
     conn = sqlite3.connect('book_catalog.db')
     cursor = conn.cursor()
 
-    # Создание таблицы для хранения книг
+    #Создание таблицы для хранения книг
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             user_name TEXT NOT NULL,
-            chat_id INTEGER NOT NULL
+            chat_id INTEGER NOT NULL,
+            UNIQUE(id, chat_id)
         )
     ''')
 
